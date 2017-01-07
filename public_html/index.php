64fc62c011e2d18dd4dff08578f39791048f7684
Unknown <mattjdupree@gmail.com>
2017-01-06 16:59:30 -0800

    Loaded existing base site
    Pulled off of my account on the UCSB Engineering Computing Infrastructure server.

---
+++ public_html/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>PHP Test</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<p>This page is under development.</p>
+<div id="testbox"><iframe src="./tests/down.php">
+	<p>iframe failed to load! I guess your browser doesn't support iframes. Odd.</p>
+</iframe></div>
+</body>
+</html>
---
+++ public_html/tests/down.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title>Down the rabbit hole</title>
+<style>
+table, th, td {
+	border:1px solid black;
+}
+</style>
+<head>
+
+<body>
+You've succesfully made it another level deeper in the available folder structure. What does this mean?
+
+<div style = "border:1px solid black">
+	<b>Some hosting tests. Don't mind these.</b>
+	<div>
+		This segment of the document represents a PHP test. If you're here for other reasons, ignore it.
+		<?php
+			$self = 'PHP_SELF';
+			$snme = 'SERVER_NAME';
+			$shst = 'SERVER_HOST';
+			$hhst = 'HTTP_HOST';
+			$href = 'HTTP_REFERER';
+			$mnme = 'SCRIPT_NAME';
+			echo '<table>';
+			echo '<tr><th>Hello World! It seems this page got PHP\'d, at least a little.</th></tr>';
+			echo '<tr><td>';
+			echo $self.":".$_SERVER[$self];
+			echo '</td></tr><tr><td>';
+			echo $snme.":".$_SERVER[$snme];
+			echo '</td></tr><tr><td>';
+			echo $shst.":".$_SERVER[$shst];
+			echo '</td></tr><tr><td>';
+			echo $hhst.":".$_SERVER[$hhst];
+			echo '</td></tr><tr><td>';
+			echo $href.":".$_SERVER[$href];
+			echo '</td></tr><tr><td>';
+			echo $mnme.":".$_SERVER[$mnme];
+			echo '</td></tr></table>';
+		?>
+	</div>
+	<br>
+	<div>
+		Website size testing section. Ignore this, too, other internetees.
+		<a href="./down.php">link</a>
+	</div>
+</div>
+
+</body>
+</html>

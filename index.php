a197605674a5b489cca6ded8e95e98b37621e491
Matthew Dupree <mattjdupree@gmail.com>
2020-09-01 18:48:12 -0700

    Add PurpleAir widget to index.php
    Section out a spot for COVID-19 info and CA fire hermitry stuff, embed PurpleAir widget inside that.

--- index.php
+++ index.php
@@ -20,6 +20,15 @@
             <p>My topics of study include computer graphics, ML, and VLSI.</p>
         </section>
         <section>
+            <h3>COVID-19 and California fire hermitry</h3>
+            <p>Here's the data from the PurpleAir sensor closest to my home. 
+            Data here is not using the LRAPA correction for 2.5um laser counters
+            exposed to 1.5um wood smoke. In layman's terms: It might be an overestimate.</p>
+            <div id='PurpleAirWidget_60509_module_AQI_conversion_C0_average_10_layer_standard'>Loading PurpleAir Widget...</div>
+            <script src='https://www.purpleair.com/pa.widget.js?key=9OFZ5QEYFCQ4MOKW&module=AQI&conversion=C0&average=10&layer=standard&container=PurpleAirWidget_60509_module_AQI_conversion_C0_average_10_layer_standard'></script>
+        </section>
+        <section>
+            <h3>Other things</h3>
             <p>While you're here, take a look at my work in <a href="classes">class</a>,
                 <a href="gamedev.php">game development</a> side projects, and
                 <a href="fanfiction.php">fanfiction</a>.

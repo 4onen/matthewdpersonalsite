bec78dc8fc7f678a76848073669a3c26ee9d0202
Matthew Dupree <mattjdupree@gmail.com>
2020-09-25 23:45:09 -0700

    Add sliding tiles puzzle game to gamedev page.

--- gamedev/index.php
+++ gamedev/index.php
@@ -20,6 +20,22 @@
                 I have yet to find time to dive into serious development,
                 
             </p>
+            <hr/>
+            <p>Driven inside by the 2020 COVID-19 pandemic and California
+                wildfires, I experimented in functional algorithms by creating
+                a clone of the classic sliding tiles puzzle game in Elm.
+                The sliding tiles puzzle is a notable challenge because, in the
+                set of all randomly initialized sets of square grids,
+                approximately half of all grids form unsolvable puzzles.
+            </p>
+            <p>My implementation is guaranteed to produce correct puzzles for all
+                seeds. If it generates an unsolvable puzzle, before presenting it,
+                it swaps the first two tiles to create the nearest puzzle in the
+                space of solvable challenges.
+            </p>
+            <iframe width="640" height="360" src="https://4onen.github.io/SlidingTilesPuzzle" frameborder="0"></iframe>
+            <p>(If the above iframe doesn't work, try it on its <a target="blank_" href="https://4onen.github.io/SlidingTilePuzzle">GitHub Pages page</a>)</p>
+            <hr/>
             <p>In Fall of 2018, I studied abroad at Lunds University.
                 For my final project in EDAF80, the Lunds Uni course on
                 Computer Graphics, I produced the title ArcticWorld, which is
@@ -30,6 +46,7 @@
                 the least TLC.
             </p>
             <iframe width="640" height="360" src="https://www.youtube.com/embed/M8tZMrgkIY0" frameborder="0" allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen></iframe>
+            <hr/>
             <p>During my study abroad at Lunds University, Sweden, 
                 I also found time to participate in the latter half of the 
                 September 2018 JS13k competitition (I started late on September 13th)
@@ -43,6 +60,7 @@
             </p>
             <p>Click the image below to go to the competition entry!</p>
             <a target="blank_" href="https://js13kgames.com/entries/peaceful-forest-walk"><img class="brightenim flexim" src="PeacefulForestWalkSS.png" alt="Screenshot of a forest from my JS13K entry Peaceful Forest Walk"/></a>
+            <hr/>
             <p>Also during my study abroad, I participated in the September 2018
                 <a target="blank_" href="https://www.meetup.com/tretton37-Tech-Meetup-Stockholm/events/253971995/">Game Jam: Part Deux</a> 
                 game dev challenge, where we had 30 hours to build a game. My
@@ -60,6 +78,7 @@
             warning: the constant camera rotation can be nauseating for some people.
             </p>
             <a target="blank_" href="https://github.com/4onen/super-duper-fiesta"><img class="flexim" src="sdf3.jpg" alt="Screenshot of 3 dice from my Game Jam Part Deux game: Super Duper Fiesta"/></a>
+            <hr/>
             <p>Prior to university, in March 2015, I and two friends 
                 participated in the first educational silicon valley 
                 Teen Hackathon, producing the physics-based game prototype 
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <article id="pageContent">
         <header>
             <h1 class="nomarg bigheader">MatthewD's Engineering Website</h1>
-            <h1 class="nomarg medheader">Computer Engineering, Analysis, and Trickery.</h2>
+            <h1 class="nomarg medheader">Computer Engineering and Trickery.</h2>
             <h1 class="nomarg smallheader">plus (<a href="fanfiction.php">fan</a>)fiction writing, roleplaying, etc...</h4>
         </header>
         <section>
@@ -20,17 +20,14 @@
             <p>My topics of study include computer graphics, ML, and VLSI.</p>
         </section>
         <section>
-            <h3>COVID-19 and California fire hermitry</h3>
-            <p>Here's the data from the PurpleAir sensor closest to my home. 
-            Data here is not using the LRAPA correction for 2.5um laser counters
-            exposed to 1.5um wood smoke. In layman's terms: It might be an overestimate.</p>
-            <div id='PurpleAirWidget_60509_module_AQI_conversion_C0_average_10_layer_standard'>Loading PurpleAir Widget...</div>
-            <script src='https://www.purpleair.com/pa.widget.js?key=9OFZ5QEYFCQ4MOKW&module=AQI&conversion=C0&average=10&layer=standard&container=PurpleAirWidget_60509_module_AQI_conversion_C0_average_10_layer_standard'></script>
+            <h3>Recent Projects</h3>
+            <p>As an experiment in functional algorithms, I recently created a clone of the classic sliding tiles puzzle game.</p>
+            <p>Check it out over on the <a href="gamedev/">game development</a> page!</p>
         </section>
         <section>
             <h3>Other things</h3>
-            <p>While you're here, take a look at my work in <a href="classes">class</a>,
-                <a href="gamedev.php">game development</a> side projects, and
+            <p>While you're here, take a look at my work in <a href="classes/">class</a>,
+                <a href="gamedev/">game development</a> side projects, and
                 <a href="fanfiction.php">fanfiction</a>.
         </section>
         <section>
